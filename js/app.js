0d83165b25c64285f344579e19511ef020fb4ab2
Jesús Rosales <jesusrosales07537@gmail.com>
2022-01-23 16:02:14 -0600

    menu responsive

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 const iconMenu = document.querySelector(".menu__icon");
+const img = document.querySelector(".menu__icon img");
 const menu = document.querySelector(".menu");
 
 iconMenu.addEventListener("click", () => {
     menu.classList.toggle("menu--active");
+
+    if(document.querySelector(".menu--active")) {
+        img.src = "img/icon-close.svg";
+    } else {
+        img.src = "img/hamburguer.svg";
+    }
 })
